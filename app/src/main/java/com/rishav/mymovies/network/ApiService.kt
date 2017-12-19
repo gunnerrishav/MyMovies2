41b94ee8ff20305057bcc7a7a1ee6c1af20e3912
gunnerrishav <gunnerrishav@gmail.com>
2017-12-19 13:30:16 +0545

    minor commit

--- app/src/main/java/com/rishav/mymovies/network/ApiService.kt
+++ app/src/main/java/com/rishav/mymovies/network/ApiService.kt
@@ -1,6 +1,7 @@
 package com.rishav.mymovies.network
 
 import com.rishav.mymovies.model.Movies
+import com.rishav.mymovies.model.popular.Popular
 import com.rishav.mymovies.util.ApiConstants
 import retrofit2.Call
 import retrofit2.http.GET
@@ -11,4 +12,7 @@ import retrofit2.http.GET
 interface ApiService {
     @GET(ApiConstants.movieList)
     fun getMovieList() : Call<Movies>
+
+    @GET(ApiConstants.popularList)
+    fun getPopularList() : Call<Popular>
 }
